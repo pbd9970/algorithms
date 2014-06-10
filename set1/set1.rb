cf6db659df261a76b03dbc1ec5ce9c16e69d7322
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-10 17:45:07 -0500

    set1 solutions with extra spec tests

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,29 @@
 module Set1
   def self.swap_small(array)
+    smallest = 0
+    array.each_index { |i| smallest = i if array[i] < array[smallest] }
+    array[smallest], array[0] = array[0], array[smallest]
+    array
   end
 
   def self.find_sum_2(array, sum = 0)
+    sum_hash = {}
+    array.each do | x |
+      sum_hash[x] = 1
+      return true unless sum_hash[sum - x].nil?
+    end
+    return false
   end
 
   def self.find_sum_3(array)
+    array.each do |x|
+      sum_hash = {}
+      array.each do |y|
+        sum_hash[y] = 1
+        puts "x=#{x}, y=#{y}"
+        return true unless sum_hash[0 - x - y].nil?
+      end
+    end
+    return false
   end
 end
