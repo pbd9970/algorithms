8d93ec37738ad11c40692f5f4cf5c971e9df03b2
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-13 16:33:52 -0500

    Initial solution commit with multiple run functionality

--- WAR/spec/war_spec.rb
+++ WAR/spec/war_spec.rb
@@ -3,3 +3,168 @@ require 'rspec'
 require 'pry-debugger'
 require_relative '../war.rb'
 
+describe 'Card' do
+  it 'exists' do
+    expect(Card).to be_a(Class)
+  end
+
+  it 'has a value' do
+    card = Card.new(13,"Spades")
+    expect(card.value).to eq(13)
+  end
+
+  it 'has a detail attribute' do
+    card2 = Card.new(7,"Diamonds")
+    expect(card2.detail).to eq("7 of Diamonds")
+  end
+end
+
+describe 'Deck' do
+  it 'exists' do
+    expect(Deck).to be_a(Class)
+  end
+  describe '.initialize' do
+
+    it 'starts as an empty deck' do
+      deck = Deck.new()
+      
+      expect(deck.deck).to eq([])
+    end
+
+    it 'can take a deck' do
+      deck = Deck.new
+      deck.create_52_card_deck
+      new_deck = Deck.new
+      new_deck.deck = deck.deck
+
+      expect(new_deck.deck).to eq(deck.deck)
+    end
+  end
+
+  describe '.create_52_card_deck' do
+    it 'can initialize a 52 card deck' do
+      deck = Deck.new
+      deck.create_52_card_deck
+
+      expect(deck.deck.size).to eq(52)
+      expect(deck.deck.first).to be_a(Card)
+    end
+  end
+
+  describe '.shuffle' do
+    it 'can shuffle the cards' do
+      deck = Deck.new
+      deck.create_52_card_deck
+      deck2 = Deck.new
+      deck2.create_52_card_deck
+
+      expect(deck.deck.map{|d| d.detail}).to eq(deck2.deck.map{|d| d.detail})
+
+      deck.shuffle
+      expect(deck.deck.size).to eq(52)
+
+      expect(deck.deck.map{|d| d.detail}).to_not eq(deck2.deck.map{|d| d.detail})
+
+      expect(deck.deck.map{|d| d.detail}.sort).to eq(deck2.deck.map{|d| d.detail}.sort)
+    end
+  end
+
+  describe '.deal_hand' do
+    it 'can deal two hands of 26 different cards' do
+      deck = Deck.new
+      deck.create_52_card_deck
+      deck.shuffle
+
+      array1, array2 = deck.deal_hand
+
+      expect(array1.size).to eq(26)
+      expect(array2.size).to eq(26)
+
+      array1.each do |card|
+        expect(array2.find{|card2| card == card2 }).to eq(nil)
+      end
+    end
+  
+    describe '.deal_card' do
+      it "can deal a card" do
+        deck = Deck.new
+        deck.create_52_card_deck
+
+        card = deck.deal_card
+
+        expect(card.detail).to eq("Ace of Spades")
+
+      end
+
+      it "will remove card from deck" do
+        deck = Deck.new
+        deck.create_52_card_deck
+
+        card = deck.deal_card
+
+        expect(deck.deck.size).to eq(51)
+      end
+    end
+
+    describe '.add_card' do
+      it 'will add a card to the deck' do
+        deck = Deck.new
+        deck.add_card([Card.new(7,"Clubs"), Card.new(8,"Diamonds")])
+        
+        expect(deck.deck.size).to eq(2)
+      end
+    end
+  end
+end
+
+describe 'Player' do
+  it 'exists' do 
+    expect(Player).to be_a(Class)
+  end
+
+  describe '.initialize' do
+    it 'has a name' do
+      player1 = Player.new("George Patton")
+
+      expect(player1.name).to eq("George Patton")
+    end
+  end
+
+  describe '.hand=' do
+    it 'can get a hand dealt' do
+      deck = Deck.new
+      deck.create_52_card_deck
+      deck.shuffle
+      hand1, hand2 = deck.deal_hand
+
+      player2 = Player.new("Erwin Rommel")
+
+      player2.hand = hand2
+
+      expect(player2.hand.deck).to eq(hand2)
+    end
+  end
+end
+
+describe 'War' do
+  it 'exists' do
+    expect(War).to be_a(Class)
+  end
+  
+  describe '.initialize' do
+    it 'creates a game' do
+      game = War.new("Patton", "Rommel")
+
+      #???
+    end
+  end
+
+  describe '.play_game' do
+    it 'plays a game of war' do
+      game = War.new("Patton", "Rommel")
+
+      game.play_game
+    end
+  end
+end
+
--- WAR/war.rb
+++ WAR/war.rb
@@ -3,48 +3,99 @@ class Card
   # Rank is the rank of the card, 2-10, J, Q, K, A
   # Value is the numeric value of the card, so J = 11, A = 14
   # Suit is the suit of the card, Spades, Diamonds, Clubs or Hearts
-  def initialize(rank, value, suit)
-    @rank = rank
-    @value = value
-    @suit = suit
-  end
+    attr_reader :value, :detail
+    @@face_card = {
+        11 => "Jack",
+        12 => "Queen",
+        13 => "King",
+        14 => "Ace" }
+
+    def initialize(value, suit)
+        @value = value
+        raise if value < 2 || value > 14
+        @detail = @@face_card[value] ? "#{@@face_card[value]}" : value.to_s
+        @detail += " of #{suit}"
+    end
 end
 
-# TODO: You will need to complete the methods in this class
 class Deck
   attr_accessor :deck
-  def initialize
-    @deck = nil # Determine the best way to hold the cards
+
+  def initialize(deck=nil)
+    @deck = deck ? deck : Array.new()
+    @discard = Array.new()
+  end
+
+  def deck
+    @deck + @discard.reverse!
+  end
+
+  def deck_size
+    @deck.size + @discard.size
   end
 
   # Given a card, insert it on the bottom your deck
-  def add_card(card)
-
+  def add_card(card_array)
+    card_array.each { |card| @discard << card }
   end
 
   # Mix around the order of the cards in your deck
   def shuffle # You can't use .shuffle!
+    shuffled_deck = []
+    (@deck.size-1).downto(1).each do | size |
+      index = rand(size)
+      shuffled_deck << @deck[index]
+      @deck[index] = @deck[size]
+    end
+    shuffled_deck << @deck[0]
+    @deck = shuffled_deck
 
+  end
+
+  def deal_hand
+    return @deck.pop(@deck.size/2), @deck
   end
 
   # Remove the top card from your deck and return it
   def deal_card
+    if @deck.size <= 1
+      @deck = @discard.reverse! + @deck
+      @discard = Array.new
+    end
 
+    @deck.pop
   end
 
   # Reset this deck with 52 cards
   def create_52_card_deck
-
+    new_deck = Array.new()
+    values = Array(2..14)
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    suits.each do | suit |
+      values.each do | value |
+        new_deck << Card.new(value,suit)
+      end
+    end
+    @deck = new_deck
   end
+end
 
+class Card
 end
 
 # You may or may not need to alter this class
 class Player
+  attr_reader :name, :hand
+
   def initialize(name)
     @name = name
     @hand = Deck.new
   end
+
+  def hand=(hand)
+    @hand.deck = hand
+  end
+
 end
 
 
@@ -53,12 +104,54 @@ class War
     @deck = Deck.new
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
-    # You will need to shuffle and pass out the cards to each player
+    
   end
 
   # You will need to play the entire game in this method using the WarAPI
-  def play_game
+  def play_game(run_game=1)
     # WarAPI.play_turn()
+    start = Time.now
+    max_times = 0
+    min_times = 999999
+    score = [0, 0]
+    avg_counter = 0
+    run_game.times do
+      counter = 0
+      # You will need to shuffle and pass out the cards to each player
+      @deck.create_52_card_deck()
+      @deck.shuffle()
+
+      @player1.hand, @player2.hand = @deck.deal_hand()
+      
+      while true
+        counter += 1
+        #print counter, "  --  P1: ", @player1.hand.deck_size, " P2: ", @player2.hand.deck_size, "\n"
+        if @player1.hand.deck_size == 0 || @player2.hand.deck_size == 0
+          if @player1.hand.deck_size == 0
+            score[1] += 1
+          else
+            score[0] += 1
+          end
+          break
+        end
+        return_hand = WarAPI.play_turn(@player1.name, @player1.hand.deal_card, @player2.name, @player2.hand.deal_card)
+        @player1.hand.add_card(return_hand[@player1.name])
+        @player2.hand.add_card(return_hand[@player2.name])
+      end
+      
+      max_times = counter if counter > max_times
+      min_times = counter if counter < min_times
+      avg_counter += counter
+
+    end
+
+    avg_time = (Time.now - start) / run_game
+    puts "Average time: #{avg_time}"
+    puts "Average moves: #{avg_counter / run_game}"
+    puts "Max: #{max_times}"
+    puts "Min: #{min_times}"
+    puts "Final Score:"
+    puts "#{@player1.name} #{score.join(" | ")} #{@player2.name}"
   end
 end
 
@@ -67,9 +160,11 @@ class WarAPI
   # This method will take a card from each player and
   # return a hash with the cards that each player should receive
   def self.play_turn(player1, card1, player2, card2)
+    binding.pry unless card2
+    binding.pry unless card1
     if card1.value > card2.value
       {player1 => [card1, card2], player2 => []}
-    elsif card2.value > card1.value || Rand(100).even?
+    elsif card2.value > card1.value || rand(100).even?
       {player1 => [], player2 => [card2, card1]}
     else
       {player1 => [card1, card2], player2 => []}
