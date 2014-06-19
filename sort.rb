8235dcb7be3e95b2534801f066fe14ba76466bd1
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-19 16:51:50 -0500

    Added merge sort functionality and test

--- sort.rb
+++ sort.rb
@@ -9,4 +9,27 @@ module Sort
     end
     return array
   end
+
+  def self.merge_sort(array)
+    return array if array.size <= 1
+    split = array.size / 2
+    a, b = merge_sort(array.pop(split)), merge_sort(array)
+    a_counter = b_counter = 0
+    solution = []
+    sort = lambda { |a,b| return (a < b) ? (a_counter += 1) && a : (b_counter += 1) && b }
+    (a.size + b.size).times do
+      if a_counter >= a.size
+        solution += b[b_counter..-1]
+        break
+      else
+        if b_counter >= b.size
+          solution += a[a_counter..-1]
+          break
+        else
+          solution << sort.call(a[a_counter], b[b_counter])
+        end
+      end
+    end
+    solution
+  end
 end
