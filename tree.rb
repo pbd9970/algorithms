0d014e1e31bfe223e1baf289d70fe5f8f6d3f51e
Paul Dombkowski <pdombkowski@gmail.com>
2014-07-10 14:45:05 -0500

    Added my attempt at iterative solution and patrick's solution

--- tree.rb
+++ tree.rb
@@ -28,45 +28,53 @@ class BinaryTree
     node.add_child(value)
   end
 
-  def tree_array(node)
-    return_array = []
-    return_array + tree_array(node.left_child) unless node.left_child.nil?
-    return_array << node.value
-    return_array + tree_array(node.right_child) unless node.right_child.nil?
-    return_array
-  end
-
-  def self.tree_constructor(array)
-    head_index = array.length / 2
-    tree = BinaryTree.new(array[head_index])
-
-    left_side  = array.slice(0..head_index-1)
-    right_side = array.slice(head_index+1..-1)
-
-    tree.head.left_child  = __tree_constructor__(left_side)  unless left_side.empty?
-    tree.head.right_child = __tree_constructor__(right_side) unless right_side.empty?
-    tree
-  end
-
-  def self.__tree_constructor__(array)
-    raise KeyError, "Node cannot be nil" if array.empty?
-    node_index = array.length / 2
-    node = Node.new(array[node_index])
-    return node if array.length <= 1
-   
-    left_side  = node_index == 0 ? [] : array.slice(0..node_index-1)
-    right_side = array.slice(node_index+1..-1)
-
-    node.left_child  = __tree_constructor__(left_side)  unless left_side.empty?
-    node.right_child = __tree_constructor__(right_side) unless right_side.empty?
-    node
-  end
-    
   def rebalance_tree
-    values = tree_array(@head)
+    array = [@head]
+    array.each do |element|
+      array.push(element.left_child) if element.left_child
+    array.each do |element|
+      array.push(element.right_child) if element.right_child
 
-    tree_constructor(values)
-  end
+
+# def tree_array(node)
+#   return_array = []
+#   return_array + tree_array(node.left_child) unless node.left_child.nil?
+#   return_array << node.value
+#   return_array + tree_array(node.right_child) unless node.right_child.nil?
+#   return_array
+# end
+
+# def self.tree_constructor(array)
+#   head_index = array.length / 2
+#   tree = BinaryTree.new(array[head_index])
+
+#   left_side  = array.slice(0..head_index-1)
+#   right_side = array.slice(head_index+1..-1)
+
+#   tree.head.left_child  = __tree_constructor__(left_side)  unless left_side.empty?
+#   tree.head.right_child = __tree_constructor__(right_side) unless right_side.empty?
+#   tree
+# end
+
+# def self.__tree_constructor__(array)
+#   raise KeyError, "Node cannot be nil" if array.empty?
+#   node_index = array.length / 2
+#   node = Node.new(array[node_index])
+#   return node if array.length <= 1
+#  
+#   left_side  = node_index == 0 ? [] : array.slice(0..node_index-1)
+#   right_side = array.slice(node_index+1..-1)
+
+#   node.left_child  = __tree_constructor__(left_side)  unless left_side.empty?
+#   node.right_child = __tree_constructor__(right_side) unless right_side.empty?
+#   node
+# end
+#   
+# def rebalance_tree
+#   values = tree_array(@head)
+
+#   tree_constructor(values)
+# end
 end
 
 class Node
